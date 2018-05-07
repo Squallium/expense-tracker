23fbb5acaf2c868ba6841b3e9a495326cc734661
Squallium <squallium3@gmail.com>
2018-05-07 17:56:05 +0200

    now trying meteor

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    db: {
-        production: process.env.MONGOLAB_URI,
-        development: "mongodb://localhost/expense-tracker-dev",
-        test: "mongodb://localhost/expense-tracker-test",
-    }
-};
