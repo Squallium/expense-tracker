f4b9f2329edfee1e33ab40294628e49cca3c2059
Squallium <squallium3@gmail.com>
2018-09-03 02:18:19 +0200

    ahora si esta como en el ejemplo

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.html',
-    styleUrls: ['./app.scss']
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class MyApp {
     title = 'untitled';
