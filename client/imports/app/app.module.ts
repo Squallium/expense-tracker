6c3a8da17aa95d535a9263323c1044e4b98bd131
Squallium <squallium3@gmail.com>
2018-09-04 15:45:54 +0200

    funcionan las tablas XD

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { MyApp } from './app.component';
 import {BrowserModule} from "@angular/platform-browser";
 
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+
 @NgModule({
     declarations: [
         MyApp
     ],
     imports: [
+        NgxDatatableModule,
         BrowserModule
     ],
     providers: [],
